d6c8b987349da6ba1259a55a625f7d07f6bba978
alanllamas <alanllamasg@gmail.com>
2022-08-22 14:48:48 -0500

    chore: removed bootstrap and adde material and tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { BillsModule } from './bills/bills.module';
 import { SpentsModule } from './spents/spents.module';
 import { UtilsModule } from './utils/utils.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { UtilsModule } from './utils/utils.module';
     AppRoutingModule,
     BillsModule,
     SpentsModule,
-    UtilsModule
+    UtilsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
