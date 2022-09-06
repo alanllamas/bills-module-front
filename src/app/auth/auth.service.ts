9bc06cf619a89d34bdc42c27fb0c827a6c94947a
alanllamas <alanllamasg@gmail.com>
2022-09-06 15:02:17 -0500

    chore: removed comments

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -16,27 +16,7 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
 
   @Dispatch() login = (token, expirationTime, refreshToken) => new SetToken(token, expirationTime, refreshToken);
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  // firebaseConfig =
-  // {
-  //   apiKey: "AIzaSyDHG0_TlvCq5YWy81tfR0xzuwhicjIgfPw",
-  //   authDomain: "itacate-10070.firebaseapp.com",
-  //   projectId: "itacate-10070",
-  //   storageBucket: "itacate-10070.appspot.com",
-  //   messagingSenderId: "1090728081415",
-  //   appId: "1:1090728081415:web:d8cfd8ec824ecf5d9e52a2",
-  //   measurementId: "G-FDFRPGF8R9"
-  // };
-
-  // databaseURL: "FIREBASE_DATABASE_URL"
-
-  // appID: "1:1090728081415:web:d8cfd8ec824ecf5d9e52a2"
-
-  // Initialize Firebase
+ 
   app = initializeApp(environment.FirebaseConfig);
   analytics = getAnalytics(this.app);
   auth = getAuth(this.app)
