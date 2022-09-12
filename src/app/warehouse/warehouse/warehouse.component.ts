f3a5a5e0aefa1b37cffb463d30605e2dedf80c75
alanllamas <alanllamasg@gmail.com>
2022-09-12 01:29:53 -0500

    feat: added selects and data from state to qr code

--- src/app/warehouse/warehouse/warehouse.component.ts
+++ src/app/warehouse/warehouse/warehouse.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { WarehouseState } from 'src/app/states/warehouse.state';
 
 @Component({
   selector: 'app-warehouse',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./warehouse.component.scss']
 })
 export class WarehouseComponent implements OnInit {
-
+  @Select(WarehouseState.ProductList) products: Observable<any[]>
   constructor() { }
   output
   ngOnInit(): void {
+  }
+  getData(e) {
+    if (e) {
+      console.log(e);
+      
+      this.output = e
+    }
   }
 
 }
