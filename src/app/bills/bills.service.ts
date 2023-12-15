578b7f047b30a1fa8b36132f71bf88333108f56f
alan llamas <alanllamasg@gmail.com>
2023-12-15 11:33:58 -0600

    choreÑ added one col to call

--- src/app/bills/bills.service.ts
+++ src/app/bills/bills.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class BillsService {
   spreadsheetId = '1B7Rl-OpAZJG6WPPl5k-LKtZTwt7uZapoM4jUMhAI-mE'
-  range = 'Full Data!A1:BA'
+  range = 'Full Data!A1:BB'
   apiKey = 'AIzaSyAg6Uxjg6_SznPVuNipzDwv0adNZa8bl7k'
 
   googleForm = `https://sheets.googleapis.com/v4/spreadsheets/${this.spreadsheetId}/values/${this.range}?key=${this.apiKey}`
